74668237abfadbd99d295d2a2262f0e42e0ea802
Q0x18c75bcf <48421711+Q0x18c75bcf@users.noreply.github.com>
2020-02-22 12:33:55 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,3 +9,4 @@
 sed -i 's/192.168.1.1/192.168.101.22/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/luci-app-xlnetacc//g' openwrt/target/linux/x86/Makefile
 sed -i 's/luci-app-zerotier//g' openwrt/target/linux/x86/Makefile
+sed -i 's/luci-app-zerotier//g' openwrt/include/target.mk
