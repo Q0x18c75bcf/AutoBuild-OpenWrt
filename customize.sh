1955315f5b2fc5895cfd581c99d229e1a513aaae
Q0x18c75bcf <48421711+Q0x18c75bcf@users.noreply.github.com>
2020-05-10 23:28:17 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,4 +9,10 @@
 sed -i 's/192.168.1.1/192.168.101.22/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/luci-app-xlnetacc//g' openwrt/target/linux/x86/Makefile
 sed -i 's/luci-app-zerotier//g' openwrt/target/linux/x86/Makefile
-sed -i 's/luci-app-zerotier//g' openwrt/include/target.mk
+sed -i 's/luci-app-openvpn-server//g' openwrt/target/linux/x86/Makefile
+sed -i 's/luci-app-amule//g' openwrt/target/linux/x86/Makefile
+sed -i 's/luci-app-qbittorrent//g' openwrt/target/linux/x86/Makefile
+sed -i 's/luci-app-ipsec-vpnd//g' openwrt/target/linux/x86/Makefile
+sed -i 's/luci-app-music-remote-center//g' openwrt/target/linux/x86/Makefile
+sed -i 's/luci-app-vlmcsd//g' openwrt/include/target.mk
+
